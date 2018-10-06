65b4cbafccf3fff2d288f4bc52fd0ea02056df23
Kristian <huskrist@hotmail.com>
2018-10-06 14:03:05 +0200

    Added initial styling to motivationmanager, tested only for android devices. #7

--- screens/MotivationScreen.js
+++ screens/MotivationScreen.js
@@ -3,6 +3,7 @@ import {
     Text,
     View,
     ListView,
+    StyleSheet,
     Modal,
     Button,
     TouchableHighlight,
@@ -91,9 +92,10 @@ export default class MotivationScreen extends React.Component {
         return (
             <TouchableHighlight
             onPress = {() => this.onRowPress(rowID)}
+            style={styles.listElement}
             underlayColor="#33aaddaa"
             >
-                <Text>{rowData}</Text>
+                <Text style={styles.listText}>{rowData}</Text>
             </TouchableHighlight>
         );
     }
@@ -101,48 +103,55 @@ export default class MotivationScreen extends React.Component {
 
   render() {
     return (
-    <View>
-        <View style>
+    <View style={styles.pageContainer}>
+        <View style={styles.container}>
 
             <TouchableHighlight
                 onPress={this.onPressNewGoal}
+                style = {styles.newElementButton}
 
             >
-            <Text>Legg til nytt mål</Text>
+            <Text style={styles.buttonText}>Legg til nytt mål</Text>
             </TouchableHighlight>
         
             <ListView
             dataSource={this.state.dataSource}
             renderRow={this.renderListRow}
+            style={styles.listElementContainer}
             />
         </View>
         <Modal
             transparent={true}
             animationType = 'slide' // fade or none
             visible = {this.state.modalVisible}
+            style = {styles.modal}
             onRequestClose={() => {
                 console.log("Request to close modal");
             }}>
-            <View>
-                <Text>Nytt mål</Text>
+            <View style={styles.modal}>
+                <Text style={styles.modalTitle}>Nytt mål</Text>
                 <TextInput
+                    style={styles.modalInput}
                     placeholder = 'Målbeskrivelse'
                     onChangeText={(modalInput) => this.setState({modalInput})}
                     value={this.state.modalInput}/>
                 
-                <View>
+                <View style={styles.modalButtons}>
                     <TouchableHighlight
                         onPress={this.closeAndCreate}
+                        style = {styles.modalCreateButton}
                         underlayColor="#00000022">
-                        <Text>Opprett</Text>
+                        <Text style={styles.buttonText}>Opprett</Text>
                     </TouchableHighlight>
                     <TouchableHighlight
                         onPress={this.cancelModal}
+                        style = {styles.modalCancelButton}
                         underlayColor="#aaaaaaaa">
-                        <Text>Avbryt</Text>
+                        <Text style={styles.buttonText}>Avbryt</Text>
                     </TouchableHighlight>
                 {this.state.editGoal && <Button
                 onPress={this.deleteElement}
+                style={styles.modalDeleteButton}
                 title="Slett"
                 />}
                 </View>
@@ -156,3 +165,98 @@ export default class MotivationScreen extends React.Component {
   }
 }
 
+const styles = StyleSheet.create({
+    pageContainer: {
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        backgroundColor: '#fff',
+    },
+    container: {
+      backgroundColor: '#fff',
+      marginHorizontal: 20,
+    }, 
+    newElementButton: {
+       backgroundColor: "rgba(40,200,40, 1)",
+       paddingVertical: 20,
+       marginVertical: 10,
+       borderRadius: 10,
+       elevation: 3,    // android only
+    }, 
+    listElementContainer: {
+        marginBottom: 100,
+    }, 
+
+    listElement: {
+        flex: 1,
+        flexDirection: 'row',
+        backgroundColor: '#EEEEEE',
+        borderStyle: 'solid',
+        borderColor: '#DDDDDD',
+        borderWidth: 2,
+        borderRadius: 5,
+        height: 50,
+        marginBottom: 5,
+        
+        
+    }, 
+    listText: {
+        fontSize: 20,
+        textAlignVertical: 'center',
+        paddingLeft: 30,
+    }, 
+    modal: {
+        flex: 1,
+        backgroundColor: "#ffffff",
+        marginHorizontal: 20,
+        marginVertical: 25,
+        elevation: 20, // android only
+    },
+    modalButtons: {
+        marginHorizontal: 20,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        height: 100,
+    },
+    modalInput: {
+        marginHorizontal: 30,
+        borderRadius: 5,
+        height: 30,
+        paddingHorizontal: 5,
+        marginBottom: 30,
+        marginVertical: 20,
+        paddingBottom: 5,
+        fontSize: 20,
+    },
+    modalTitle: {
+        alignItems: 'center',
+        textAlign: 'center',
+        fontSize: 16,
+    },
+    buttonText: {
+        fontSize: 24,
+        color: "#fff",
+        textAlign: 'center',
+    },
+    modalCreateButton: {
+        backgroundColor: "rgba(40,200,40, 1)",
+        paddingVertical: 20,
+        borderRadius: 10,
+        width: 120,
+        elevation: 3,    // android only
+    },
+    modalCancelButton: {
+        backgroundColor: "rgba(255,200,40, 1)",
+        paddingVertical: 20,
+        borderRadius: 10,
+        width: 120,
+        elevation: 3,    // android only
+    },
+    modalDeleteButton: {
+        backgroundColor: "rgba(200, 40,40, 1)",
+        paddingVertical: 20,
+        borderRadius: 10,
+        elevation: 3,    // android only
+    },
+        
+});
