472d26a13e9e6f8849f6c1cd7f2eb48346c5490a
Balázs Orbán <info@balazsorban.com>
2018-10-08 19:47:59 +0200

    💄 add extra colors
    #8 #7

--- constants/Colors.js
+++ constants/Colors.js
@@ -11,4 +11,12 @@ export default {
   warningText: '#666804',
   noticeBackground: tintColor,
   noticeText: '#fff',
+  color1: "#051e3e",
+  color2: "#251e3e",
+  color3: "#451e3e",
+  color4: "#651e3e",
+  color5: "#851e3e",
+  dark: "#444",
+  light: "#ededed",
+  lightgrey: "#ddd"
 };
