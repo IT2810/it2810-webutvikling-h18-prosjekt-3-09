00a5a969b0a06ce5d1c68d14874d6796c7c4ef7b
Unknown <haavard.b.olsen@gmail.com>
2018-10-09 15:42:40 +0200

    Update ui and improved code structure #1 #7

--- components/Note.js
+++ components/Note.js
@@ -1,55 +1,37 @@
 import React, { Component } from "react";
 import { StyleSheet, Text, View } from "react-native";
-import { Icon } from "react-native-elements";
+import { Icon, Card, Button, Divider } from "react-native-elements";
+import Colors from "../constants/Colors";
 
 export default class Note extends Component {
   render() {
     const { id, title, data, deleteMethod, editNote } = this.props;
     return (
-      <View id={id} style={styles.note}>
-        <Text style={styles.noteTitle}>{title}</Text>
-        <Text style={styles.noteText}>{data}</Text>
-        <Icon
-          name="delete"
-          color="#e5547b"
-          onPress={() => deleteMethod(id)}
-          accessibilityLabel="Press to remove task from list"
-          underlayColor="rgba(0,0,0,0)"
+      <Card containerStyle={{ borderRadius: 6 }}>
+        <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
+          <View />
+          <Text style={{ fontWeight: "bold", flexGrow: 1 }}>{title}</Text>
+          <Icon
+            name="delete"
+            color="#e5547b"
+            onPress={() => deleteMethod(id)}
+            accessibilityLabel="Press to remove task from list"
+            underlayColor="rgba(0,0,0,0)"
+          />
+        </View>
+        <Divider
+          style={{
+            backgroundColor: Colors.lightgrey,
+            marginTop: 5,
+            marginBottom: 5
+          }}
         />
-        <Icon raised name="edit" color="#f50" onPress={editNote} />
-      </View>
+        <Text style={{ marginBottom: 10 }}>{data}</Text>
+
+        <Icon size={12} raised name="edit" color="#f50" onPress={editNote} />
+      </Card>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  note: {
-    flex: 1,
-    height: 170,
-    padding: 5,
-    margin: 2,
-    borderWidth: 2
-  },
-  noteTitle: {
-    fontWeight: "bold"
-  },
-  noteText: {
-    paddingLeft: 20,
-    borderLeftWidth: 10
-  },
-  noteDeleteText: {
-    color: "black",
-    fontWeight: "bold"
-  },
-  noteDelete: {
-    borderWidth: 1,
-    borderColor: '"rgba(0,0,0,0.2)"',
-    alignItems: "center",
-    justifyContent: "center",
-    width: 20,
-    height: 20,
-    left: 5,
-    backgroundColor: "#e5547b",
-    borderRadius: 20
-  }
-});
+const styles = StyleSheet.create({});
